a398699a51407581d5ed1d71e080eb823ae31607
Marton Lajos <lajosmarton@hotmail.com>
2025-01-15 09:06:21 +0100

    kicsivel jutottam csak tovább

--- Worksheet/MainPage.xaml.cs
+++ Worksheet/MainPage.xaml.cs
@@ -1,24 +1,32 @@
-﻿namespace Worksheet
+﻿using System.Collections.ObjectModel;
+
+namespace Worksheet;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+    ObservableCollection<WorkSheet> Worksheets = new();
+
+    public MainPage()
     {
-        List<WorkSheet> worksheets;
+        InitializeComponent();
 
-        public MainPage()
-        {
-            InitializeComponent();
+        BrandPicker.ItemsSource = Enum.GetValues<Brands>();
+        CreateDate.MinimumDate = DateTime.Now.AddYears(-1);
+        CreateDate.MaximumDate = DateTime.Now;
+        CreateDate.Date = DateTime.Now;
 
-            BrandPicker.ItemsSource = Enum.GetValues<Brands>();
-            CreateDate.MinimumDate = DateTime.Now.AddYears(-1);
-            CreateDate.MaximumDate = DateTime.Now;
-            CreateDate.Date = DateTime.Now;
-        }
+        Worksheets.Add(new WorkSheet {
+            Brand = Brands.Audi, LicensePlate = "AAA111", Id = 1, OdoMeter = 20000 });
+        WorkList.ItemsSource = Worksheets;
+    }
 
-        private void OnSaveClicked(object sender, EventArgs e)
-        {
-            //TODO: get data from Entries, Pickers
-            //TODO: Create new WorkSheet
-            //TODO: Add worksheet to workSheets
-        }
+    private void OnSaveClicked(object sender, EventArgs e)
+    {
+        var newWorksheet = new WorkSheet();
+        newWorksheet.Brand = (Brands)BrandPicker.SelectedItem;
+        newWorksheet.OdoMeter = int.Parse(Odometer.Text);
+        newWorksheet.LicensePlate = LicensePlate.Text;
+
+        Worksheets.Add(newWorksheet);
     }
 }
